9a7ddf711bda69c07b348494a684c31e0b1d88df
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2026-03-29 10:11:45 +0200

    fix: change semantics; styles: changed values for colors

--- src/Components/Chat/Message/UserMessageItem.tsx
+++ src/Components/Chat/Message/UserMessageItem.tsx
@@ -2,21 +2,20 @@ import { SquarePen } from "lucide-react";
 
 const UserMessageItem = ({ message }: { message: string }) => {
   return (
-    <div className="flex justify-end items-center gap-6">
-      <div className="flex items-end max-w-[70%]"></div>
+    <li className="flex ml-auto items-center gap-2 lg:gap-6 max-w-[90%]">
       <button
-        className="size-5 md:size-10 border border-[#BFBDBD] rounded-lg text-[#7c7676] flex items-center justify-center hover:cursor-pointer"
+        className="size-5 md:size-10 border border-(--editMessageBorder) rounded-lg text-(--editMessageIcon) flex items-center justify-center hover:cursor-pointer"
         type="button"
         aria-label="Edit button"
       >
         <SquarePen />
       </button>
 
-      <div className="px-4 py-2 md:px-10 md:py-4 bg-white rounded-2xl max-w-[70%] flex items-center gap-6">
+      <div className="px-4 py-2 md:px-10 md:py-4 bg-white rounded-2xl  flex items-center gap-6 w-full">
         <img src="/user_photo.png" alt="user photo" className="size-10" />
-        <p className="break-all">{message}</p>
+        <p className="break-all text-xs lg:text-lg w-full">{message}</p>
       </div>
-    </div>
+    </li>
   );
 };
 

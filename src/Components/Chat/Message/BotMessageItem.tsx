f3d5d1c716eb715d8ff45c2e92655826b967ae24
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2026-03-29 10:09:58 +0200

    feat + style: make functions for copy and speech buttons and updated styles to respond to new textarea behavior

--- src/Components/Chat/Message/BotMessageItem.tsx
+++ src/Components/Chat/Message/BotMessageItem.tsx
@@ -1,23 +1,34 @@
 import {
   Clipboard,
+  Pause,
   RefreshCcw,
   ThumbsDown,
   ThumbsUp,
   Volume2,
 } from "lucide-react";
+import { useState } from "react";
+import { useSpeechSynthesis } from "react-speech-kit";
+import { toastSuccess } from "../../Toast/Toasts";
 
 const Button = ({
   children,
   label,
+  func,
 }: {
   children: React.ReactNode;
   label: string;
+  func?: () => void;
 }) => {
   return (
     <button
       type="button"
-      className="size-4 min-[425px]:size-6 text-[#7c7676] hover:cursor-pointer"
+      className="size-4 min-[425px]:size-6 text-[#7c7676] hover:cursor-pointer flex items-center justify-center"
       aria-label={label}
+      onClick={() => {
+        if (!func) return;
+
+        func();
+      }}
     >
       {children}
     </button>
@@ -31,49 +42,75 @@ const BotMessageItem = ({
   message: string;
   previousMessage: string;
 }) => {
+  const { speak, cancel } = useSpeechSynthesis();
+  const [isReading, setIsReading] = useState(false);
+
+  const listenClick = () => {
+    if (isReading) {
+      cancel();
+      setIsReading(false);
+    } else {
+      speak({ text: message });
+      setIsReading(true);
+    }
+  };
+
+  const copyClick = () => {
+    navigator.clipboard.writeText(message);
+    toastSuccess("Message copied");
+  };
+
   return (
-    <div className="grid lg:grid-cols-[1fr_auto] gap-3 lg:gap-6 items-center ">
-      <div className="px-5 lg:px-10 py-4 bg-white rounded-2xl flex flex-col">
+    <li className="grid lg:grid-cols-[1fr_auto] gap-2 lg:gap-6 items-center text-xs lg:text-lg ">
+      <div className="px-4 lg:px-8 py-4 bg-white rounded-2xl flex flex-col">
         <div className="flex items-center gap-6 ">
-          <img src="/chat_icon.png" alt="chat icon" className="size-10" />
-          <p className="break-all">{previousMessage}</p>
+          <img
+            src="/chat_icon.png"
+            alt="chat icon"
+            className="size-6 min-[425px]:size-8 lg:size-10"
+          />
+          <p className="break-all ">{previousMessage}</p>
         </div>
 
-        <hr className="mt-6 text-[#9A9A9A5E]" />
+        <hr className="mt-4 lg:mt-6 text-(--splitline)" />
 
-        <article className="mt-10 px-5 lg:px-14.5 wrap-break-word">
+        <article className="mt-6 lg:mt-10 px-4 lg:px-16 wrap-break-word">
           {message}
         </article>
       </div>
 
-      <ul className="flex lg:flex-col items-center p-6 bg-white rounded-2xl border border-white/10 gap-5 lg:gap-10 lg:p-6 w-fit">
+      <ul className="flex lg:flex-col items-center p-2 bg-white rounded-2xl border border-white/10 gap-4 lg:gap-8 lg:p-6 w-fit">
         <li>
           <Button label="Refresh button">
-            <RefreshCcw />
+            <RefreshCcw className="size-4 lg:size-6" />
           </Button>
         </li>
         <li>
           <Button label="Like button">
-            <ThumbsUp />
+            <ThumbsUp className="size-4 lg:size-6" />
           </Button>
         </li>{" "}
         <li>
           <Button label="Dislike button">
-            <ThumbsDown />
+            <ThumbsDown className="size-4 lg:size-6" />
           </Button>
         </li>{" "}
         <li>
-          <Button label="Copy button">
-            <Clipboard />
+          <Button label="Copy button" func={copyClick}>
+            <Clipboard className="size-4 lg:size-6" />
           </Button>
         </li>{" "}
         <li>
-          <Button label="Read button">
-            <Volume2 />
+          <Button label="Read button" func={listenClick}>
+            {isReading ? (
+              <Pause className="size-4 lg:size-6" />
+            ) : (
+              <Volume2 className="size-4 lg:size-6" />
+            )}
           </Button>
         </li>{" "}
       </ul>
-    </div>
+    </li>
   );
 };
 
